8ff8ecc256a000d2c82e0b57080dd486d6a0109b
Alexander K <kovalev.alexander1@gmail.com>
2020-12-13 20:20:20 +0500

    add animation

--- src/js/script.js
+++ src/js/script.js
@@ -118,6 +118,8 @@ $(document).ready(function(){
         const _href = $(this).attr("href");
         $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
         return false;
-});
+    });
+
+    new WOW().init();
 
 });           
